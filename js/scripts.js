fb16c343e0f94b36ff656b6e4918294fa4f5b9e1
Elise St Hilaire and said ahmed <said@>
2017-01-18 11:34:39 -0800

    initialize back-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 //back-end logic (business)
 
+var translator = function(userInput) {
+  userInput.forEach(function(word) {
+    word.match(/[^a-zA-Z\d\s:]/g);
+    console.log(word);
 
-
+  })
+}
 
 
 
@@ -9,7 +14,7 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var userInput = $("#user-string").val();
+    var userInput = $("#user-string").val().split(" ");
     var translation = translator(userInput);
     $("#output").text(translation);
   });
