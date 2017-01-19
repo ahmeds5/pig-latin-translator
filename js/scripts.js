b2c02d7c223176f835b2ee5e5a2a38e52e2028e5
Elise St Hilaire and said ahmed <elise@>
2017-01-18 16:28:22 -0800

    Add backend logic for when word starts with vowel vs consanant

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,31 @@
 //back-end logic (business)
 
-var translator = function(userInput) {
-  userInput.forEach(function(word) {
-    word.match(/[^a-zA-Z\d\s:]/g);
-    console.log(word);
+function translator (userInput) {
+  // var userSplit = userInput.split(" ");
+  var vowels= ["a","e","i","o","u"];
 
-  })
-}
+  // console.log(userInput[0]);
+  if (vowels.indexOf(userInput[0]) != -1 ) {
+    return userInput + "ay";
+  }
+
+  if (vowels.indexOf(userInput[0]) === -1) {
+    var firstLetter = userInput[0];
+    var restOfWord = userInput.slice(1,userInput.length);
+    return restOfWord + firstLetter + "ay";
+  }
+
+  // for (i = 0; i < userSplit.length; i += 1) {
+  //   for(j=0; j<vowels.length; j++) {
+  //     if (userSplit[i] === vowels[j]) {
+  //     var breakPoint = vowels[j];
+  //     var newSentence = userSplit.slice(breakPoint) + userSplit.slice(0,breakPoint);
+  //     return newSentence;
+  //
+  //     }
+  //   }
+  // }
+};
 
 
 
@@ -14,8 +33,9 @@ var translator = function(userInput) {
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var userInput = $("#user-string").val().split(" ");
+    var userInput = $("#user-string").val();
     var translation = translator(userInput);
     $("#output").text(translation);
+
   });
-})
+});
