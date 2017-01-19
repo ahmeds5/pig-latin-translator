a8822ad5680defa5c6adc99298c67e37c3ccb479
Elise St Hilaire and said ahmed <elise@>
2017-01-18 16:42:04 -0800

    add few changes to jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,21 @@
 //back-end logic (business)
 
 function translator (userInput) {
-  // var userSplit = userInput.split(" ");
   var vowels= ["a","e","i","o","u"];
 
-  // console.log(userInput[0]);
   if (vowels.indexOf(userInput[0]) != -1 ) {
     return userInput + "ay";
-  }
-
-  if (vowels.indexOf(userInput[0]) === -1) {
+  } else if (vowels.indexOf(userInput[1]) === -1) {
+    var secondLetter = userInput[1];
+    var firstLetter = userInput[0];
+    var restOfWord = userInput.slice(2,userInput.length);
+    return restOfWord + firstLetter + secondLetter + "ay";
+  } else if (vowels.indexOf(userInput[0]) === -1) {
     var firstLetter = userInput[0];
     var restOfWord = userInput.slice(1,userInput.length);
     return restOfWord + firstLetter + "ay";
   }
-
-  // for (i = 0; i < userSplit.length; i += 1) {
-  //   for(j=0; j<vowels.length; j++) {
-  //     if (userSplit[i] === vowels[j]) {
-  //     var breakPoint = vowels[j];
-  //     var newSentence = userSplit.slice(breakPoint) + userSplit.slice(0,breakPoint);
-  //     return newSentence;
-  //
-  //     }
-  //   }
-  // }
 };
-
-
 
 //front-end logic (user interface)
 $(document).ready(function() {
